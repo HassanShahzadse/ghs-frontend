29443e511850242231f6ce2bdc35dd64a42a138c
ChaudryHaseeb <115344371+ChaudryHaseeb@users.noreply.github.com>
2024-11-23 18:13:37 +0500

    new

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,7 +5,7 @@ import ScrollingDivs from "./ScrollingDivs";
 const LandingPage = () => {
   return (
     <div className="landing-page">
-      <h1 className="heading">Welcome to Our Landing Page</h1>
+      <h1 className="heading">Welcome</h1>
       <h2 className="sub-heading">Scroll down to see the magic!</h2>
       <ScrollingDivs/>
     </div>
--- src/components/ScrollingDivs.js
+++ src/components/ScrollingDivs.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { motion, useMotionValue, useTransform } from "framer-motion";
-// import "../App.css";
+import "../App.css";
 
 const ScrollingDivs = () => {
   const yScroll = useMotionValue(0);
 
-  const y1 = useTransform(yScroll, [0, 1000], ["0%", "-50%"]);
-  const y2 = useTransform(yScroll, [0, 1000], ["0%", "50%"]);
-  const y3 = useTransform(yScroll, [0, 1000], ["0%", "-50%"]);
+  const y1 = useTransform(yScroll, [0, 1000], ["0%", "-100%"]);
+  const y2 = useTransform(yScroll, [0, 1000], ["0%", "100%"]);
+  const y3 = useTransform(yScroll, [0, 1000], ["0%", "-100%"]);
 
   const handleScroll = () => {
     yScroll.set(window.scrollY);
@@ -20,15 +20,18 @@ const ScrollingDivs = () => {
 
   return (
     <div className="scrolling-divs-container">
+      {/* Heading above the first div */}
+
       {/* First Div */}
       <motion.div
-        className="scroll-div div-1 "
+        className="scroll-div div-1"
         style={{ y: y1 }}
       >
+      <h2 className="overlay-heading overlay-heading-1">Above the First Div</h2>
         {[...Array(5)].map((_, index) => (
           <img
             key={index}
-            src={`/assets/11/121f4b40b626d224d72ce1e7ee790dd2-scaled-1-1024x683.jpg?text=Image+${index + 1}`}
+            src={`/assets/11/2221de4b856ff83688d757a4a9df1d6a-scaled-1-1024x819.jpg?text=Image+${index + 1}`}
             alt={`Image ${index + 1}`}
           />
         ))}
@@ -42,12 +45,15 @@ const ScrollingDivs = () => {
         {[...Array(5)].map((_, index) => (
           <img
             key={index}
-            src={`/assets/11/779f14aec30378bd9abb86d2f8256787-scaled-1-1024x576.jpg?text=Image+${index + 6}`}
+            src={`/assets/11/8d135f425b4e0766950e1c120964d11b-scaled-1-1024x683.jpg?text=Image+${index + 6}`}
             alt={`Image ${index + 6}`}
           />
         ))}
       </motion.div>
 
+      {/* Heading above the third div */}
+
+      <h1 className="overlay-heading overlay-heading-2">Above the Third Div</h1>
       {/* Third Div */}
       <motion.div
         className="scroll-div div-3"
