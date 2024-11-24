28305f325749de75af1fef14e9277cf5ed88de7f
ChaudryHaseeb <115344371+ChaudryHaseeb@users.noreply.github.com>
2024-11-25 00:21:39 +0500

    adding hover effect for text

--- src/components/ScrollingDivs.js
+++ src/components/ScrollingDivs.js
@@ -19,70 +19,102 @@ const ScrollingDivs = () => {
   }, []);
 
   const image1 = [
-    
-    "/assets/11/2221de4b856ff83688d757a4a9df1d6a-scaled-1-1024x819.jpg",
-    "/assets/11/230223b249a163f3c4e12bce538aaa76-scaled-1-1024x683.jpg",
-    "/assets/11/20231213124820.jpg",
-    "/assets/11/230223b249a163f3c4e12bce538aaa76-scaled-1-1024x683.jpg",
-    "/assets/11/20231213124820.jpg",
-  ]
+    {
+      url: "/assets/11/2221de4b856ff83688d757a4a9df1d6a-scaled-1-1024x819.jpg",
+      text: "Tradition and Legacy Innovation at Heart",
+    },
+    {
+      url: "/assets/11/230223b249a163f3c4e12bce538aaa76-scaled-1-1024x683.jpg",
+      text: "Innovation at Heart Innovation at Heart",
+    },
+    {
+      url: "/assets/11/20231213124820.jpg",
+      text: "Crafting Memories Innovation at Heart",
+    },
+    {
+      url: "/assets/11/230223b249a163f3c4e12bce538aaa76-scaled-1-1024x683.jpg",
+      text: "Precision Redefined Innovation at Heart",
+    },
+    {
+      url: "/assets/11/20231213124820.jpg",
+      text: "Timeless Beauty Innovation at Heart",
+    },
+  ];
+
   const image2 = [
-    
-    "/assets/11/8d135f425b4e0766950e1c120964d11b-scaled-1-1024x683.jpg",
-    "/assets/11/image-20241108-211702-4b1c960e-2000x1333.jpg",
-    "/assets/11/8d135f425b4e0766950e1c120964d11b-scaled-1-1024x683.jpg",
-    "/assets/11/230223b249a163f3c4e12bce538aaa76-scaled-1-1024x683.jpg",
-    "/assets/11/20231213124820.jpg",
-  ]
+    {
+      url: "/assets/11/8d135f425b4e0766950e1c120964d11b-scaled-1-1024x683.jpg",
+      text: "Future Awaits Innovation at Heart",
+    },
+    {
+      url: "/assets/11/image-20241108-211702-4b1c960e-2000x1333.jpg",
+      text: "Exploring Horizons Innovation at Heart",
+    },
+    {
+      url: "/assets/11/8d135f425b4e0766950e1c120964d11b-scaled-1-1024x683.jpg",
+      text: "Innovation Unleashed Innovation at Heart",
+    },
+    {
+      url: "/assets/11/230223b249a163f3c4e12bce538aaa76-scaled-1-1024x683.jpg",
+      text: "Shaping the Future Innovation at Heart",
+    },
+    {
+      url: "/assets/11/20231213124820.jpg",
+      text: "Boundless Creativity Innovation at Heart",
+    },
+  ];
+
   const image3 = [
-    
-    "/assets/11/927957f1544d260c4ea6af34676a57d5-scaled-1-1024x683.jpg",
-    "/assets/11/230223b249a163f3c4e12bce538aaa76-scaled-1-1024x683.jpg",
-    "/assets/11/927957f1544d260c4ea6af34676a57d5-scaled-1-1024x683.jpg",
-    "/assets/11/927957f1544d260c4ea6af34676a57d5-scaled-1-1024x683.jpg",
-    "/assets/11/20231213124820.jpg",
-  ]
+    {
+      url: "/assets/11/927957f1544d260c4ea6af34676a57d5-scaled-1-1024x683.jpg",
+      text: "Inspiring Change Innovation at Heart",
+    },
+    {
+      url: "/assets/11/230223b249a163f3c4e12bce538aaa76-scaled-1-1024x683.jpg",
+      text: "Empowering VisionsInnovation at Heart",
+    },
+    {
+      url: "/assets/11/927957f1544d260c4ea6af34676a57d5-scaled-1-1024x683.jpg",
+      text: "Driven by Passion Innovation at Heart",
+    },
+    {
+      url: "/assets/11/927957f1544d260c4ea6af34676a57d5-scaled-1-1024x683.jpg",
+      text: "Redefining Possibilities Innovation at Heart",
+    },
+    {
+      url: "/assets/11/20231213124820.jpg",
+      text: "Vision in Motion Innovation at Heart",
+    },
+  ];
 
   return (
     <div className="scrolling-divs-container">
-      <motion.div
-        className="scroll-div div-1"
-        style={{ y: y1 }}
-      >
-     
+      <motion.div className="scroll-div div-1" style={{ y: y1 }}>
         {image1.map((img, index) => (
-          <div className="image-container" key={index}>
-          {index === 0 && (
-        <div className="image-overlay">Honouring Tradition.</div>
-      )}
-          <img src={img} alt={`Image ${index + 1}`} />
-        </div>
+          <div className="image-container" key={index} >
+            {index === 0 && <div className="image-overlay">Honouring Tradition.</div>}
+            <img src={img.url} alt={`Image ${index + 1}`} />
+            <div className="hover-text">{img.text}</div>
+          </div>
         ))}
-
       </motion.div>
 
-      {/* Second Div */}
-      <motion.div
-        className="scroll-div div-2"
-        style={{ y: y2 }}
-      >
+      <motion.div className="scroll-div div-2" style={{ y: y2 }}>
         {image2.map((img, index) => (
-          <img key={index} src={img} alt={`Image ${index + 1}`} />
+          <div className="image-container" key={index}>
+            <img src={img.url} alt={`Image ${index + 1}`} />
+            <div className="hover-text">{img.text}</div>
+          </div>
         ))}
       </motion.div>
 
-      {/* Third Div */}
-      <motion.div
-        className="scroll-div div-3"
-        style={{ y: y3 }}
-      >
-         {image3.map((img, index) => (
+      <motion.div className="scroll-div div-3" style={{ y: y3 }}>
+        {image3.map((img, index) => (
           <div className="image-container" key={index}>
-          {index === 0 && (
-        <div className="image-overlay">Inspiring Change</div>
-      )}
-          <img src={img} alt={`Image ${index + 1}`} />
-        </div>
+            {index === 0 && <div className="image-overlay">Inspiring Change</div>}
+            <img src={img.url} alt={`Image ${index + 1}`} />
+            <div className="hover-text" id="hover-text">{img.text}</div>
+          </div>
         ))}
       </motion.div>
     </div>
